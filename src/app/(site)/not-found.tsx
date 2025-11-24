516c924fb01023c240d477dcb981196359a37b3c
Remjest <remjest@mail.ru>
2025-11-24 22:22:17 +0300

    somethn with width

--- src/app/(site)/not-found.tsx
+++ src/app/(site)/not-found.tsx
@@ -10,7 +10,7 @@ export default function HomePage(): JSX.Element {
             <div style={{width: '100%', height: '100%', display: 'flex', 'flexDirection': 'column', 'alignItems': "center", 'justifyContent': 'center', gap: '20px', padding: '10px'}}>
                 <div style={{'fontSize': '35px', 'fontWeight': '600'}}>404</div>
                 <div style={{ 'textAlign': 'center'}}>Упс! Такой страницы не существует.</div>
-                <Button ><Link href={'/'}>Вернуться на главную</Link></Button>
+                <Link href={'/'}><Button >Вернуться на главную</Button></Link>
             </div>
         </>
     );
