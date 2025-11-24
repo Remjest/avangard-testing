98f52ab7f90e83481ecb83c2aa386aa6cf2f07c3
Remjest <remjest@mail.ru>
2025-11-24 23:34:08 +0300

    fix not-found

--- src/app/(site)/not-found.tsx
+++ src/app/(site)/not-found.tsx
@@ -7,7 +7,7 @@ export default function HomePage(): JSX.Element {
 
     return (
         <>
-            <div style={{width: '100%', height: '100%', display: 'flex', 'flexDirection': 'column', 'alignItems': "center", 'justifyContent': 'center', gap: '20px', padding: '10px'}}>
+            <div style={{width: '100%', minHeight: '500px', height: '100%', display: 'flex', 'flexDirection': 'column', 'alignItems': "center", 'justifyContent': 'center', gap: '20px', padding: '10px'}}>
                 <div style={{'fontSize': '35px', 'fontWeight': '600'}}>404</div>
                 <div style={{ 'textAlign': 'center'}}>Упс! Такой страницы не существует.</div>
                 <Link href={'/'}><Button >Вернуться на главную</Button></Link>
