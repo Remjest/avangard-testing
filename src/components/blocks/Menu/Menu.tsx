97968de816d6d292ac1ba33fa7d46199bdd0be48
Remjest <remjest@mail.ru>
2025-12-18 22:33:13 +0300

    fix parser

--- src/components/blocks/Menu/Menu.tsx
+++ src/components/blocks/Menu/Menu.tsx
@@ -83,6 +83,7 @@ function FirstLevel( props: MenuProps) {
                 setSubcategory(activeSubcat.subcategoryId);
             }
         }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [pagePath]);
     
     const variantsFirst = {
